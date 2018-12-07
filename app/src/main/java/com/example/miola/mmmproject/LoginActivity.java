42f5e23d96c0958137392f1082d3c74122be647d
Amine <sef.amine@hotmail.fr>
2018-12-07 20:17:05 +0100

    authentification

---
+++ app/src/main/java/com/example/miola/mmmproject/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.miola.mmmproject;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText emailField;
+    private EditText passwordField;
+    private FirebaseAuth fireAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        fireAuth = FirebaseAuth.getInstance();
+
+        emailField = findViewById(R.id.email);
+        passwordField = findViewById(R.id.password);
+        findViewById(R.id.login).setOnClickListener(this);
+
+
+    }
+
+
+    private void startSignIn(){
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Fields are empty", Toast.LENGTH_LONG).show();
+        }else{
+            fireAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                }
+            });
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                startSignIn();
+                break;
+        }
+    }
+}
